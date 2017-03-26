0bac88c61318413a499e1c69f65d6c83a63f7290
Robert Allen <bobbidigi@gmail.com>
2017-03-26 11:02:58 -0400

    bootrap resonvisv

--- script.js
+++ script.js
@@ -14,12 +14,9 @@ $(document).ready(function() {
            var game = data.stream.channel.game;
          }
          
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/users/"+ streamer +"?callback=?", function (data) {
-  // console.log(data)
-      // gamelink ='<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' + game + '</a>';
-      gamelink='<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >'+game+'</a>';
-      // gamelink ='<a href="https://www.twitch.tv/' + data.game + '" target="_blank" >' +data.game + '</a>';
-      namelink= '<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a>';
+$.getJSON("https://wind-bow.gomix.me/twitch-api/users/"+ streamer +"?callback=?",function (data) {
+   gamelink='<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >'+game+'</a>';
+   namelink= '<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a>';
                
        
   if (data.logo==undefined) {
@@ -32,26 +29,14 @@ $(document).ready(function() {
      $('#col').append('<div class="OFF">' +uLogo + namelink + '<span class="offline"> (offline) </span> <br><hr><br></div>');
    }else {
      $('#col').append('<div class="ON">' +uLogo + namelink + 'now playing' + gamelink + '<span class="online">(Online) </span>'+'<br><hr><br></div>');
-     // $('#col').append(uLogo + namelink + 'now playing' + gamelink + '<span class="online">(Online)</span>'+'<br><hr><br>');
-     // console.log(game);
    }
-    // $('#col').append(uLogo + namelink + 'now playing \"' + game + '\" <span class="online">(Online)</span>'+'<br><hr><br>');
-    /*$('#col').append('<img src="'+data.logo+'"> <a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a><span class="offline"> (Offline)</span> <br><hr><br>');*/
    
-    
-  // $('#col').addClass('online');  
-    
-   
-      
   if (data.status==404&&data.logo==undefined)
        $('#col').append('<div class="OFF">' + '<i class="fa fa-question-circle fa-3x" aria-hidden="true"></i>' + '<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a>' + '<span class="offline">[404]</span>' + '<br><hr><br></div>');
-      
-      // console.log(data.status);
   
   });
    });
   };
-  // for (x=0; x<5; x++) {
   for (x=0; x<streamers.length; x++) {
     fillDetails(streamers[x], [x]);
     
