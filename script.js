62083d2883d9db8ef7e975013356bc860dda293c
bobbidigi <bobbidigi34@gmail.com>
2017-03-27 11:32:00 -0400

    ...

--- script.js
+++ script.js
@@ -20,19 +20,23 @@ $.getJSON("https://wind-bow.gomix.me/twitch-api/users/"+ streamer +"?callback=?"
                
 //logo+name/////logo+name//////logo+name/////       
   if (data.logo==undefined) {
-        uLogo='<div id="logo" class="float-left col-md-3"><img class="img-rounded" width="50px" height="50px" alt="Responsive image" src="https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png"><br>'+namelink+'</div>';
+        uLogo='<div id="logo" class="float-left col-md-2"><img class="img-rounded" width="50px" height="50px" alt="Responsive image" src="https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png"><br>'+namelink+'</div>';
+      
+       }else if(data.status==404&&data.logo==undefined){
+       uLogo='<div id="logo" class="float-left col-md-2"><img class="img-rounded" width="50px" height="50px" alt="Responsive image" src="https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png"><br>'+namelink+'</div>';   
+            
         }else{
-        uLogo='<div id="logo" class="float-left col-md-3"><img class="img-rounded" width="50px" height="50px" alt="Responsive image" src="'+data.logo+'"><br>'+namelink+'</div>';
+        uLogo='<div id="logo" class="float-left col-md-2"><img class="img-rounded" width="50px" height="50px" alt="Responsive image" src="'+data.logo+'"><br>'+namelink+'</div>';
     }
  ///status//on-off//////status//on-off///
  if (status=='offline'){
      $('#col').append('<div class="OFF">'+uLogo+'<span class="offline float-right">off</span></div><br><hr><br>');
    }else {
-     $('#col').append('<div class="ON">' +uLogo+ 'now playing'  +gamelink+ '<span class="online float-right">On</span></div><br><hr><br>');
+     $('#col').append('<div class="ON">' +uLogo+ 'playing'  +gamelink+ '<span class="online float-right">On</span></div><br><hr><br>');
    }
-   
-  if (data.status==404&&data.logo==undefined)
-       $('#col').append('<div class="OFF col-md-12">' + '<i class="fa fa-question-circle fa-4x" aria-hidden="true"></i>' + '<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a>' + '<span class="offline">[404]</span>' + '<br><hr><br></div>');
+   /*if (data.status==404&&data.logo==undefined)
+       $('#col').append('<div class="OFF col-md-12">' + '<i class="fa fa-question-circle fa-4x" aria-hidden="true"></i>' + '<a href="https://www.twitch.tv/' + data.name + '" target="_blank" >' +data.display_name + '</a>' + '<span class="offline">[404]</span>' + '<br><hr><br></div>');*/
+  
   
   });
    });
